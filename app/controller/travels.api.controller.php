9b045378c3a952bcf9b5a93b649522ecf0cb3c07
joaquin <joakotroncoso.jt@gmail.com>
2023-11-12 14:18:51 -0300

    a

--- app/controller/travels.api.controller.php
+++ app/controller/travels.api.controller.php
@@ -3,15 +3,35 @@
 require_once './app/model/travels.model.php';
 require_once './app/views/api.view.php';
 
-class TravelsApiController{
+class TravelsApiController
+{
     private $model;
     private $view;
-    function __construct(){
+    function __construct()
+    {
         $this->model = new TravelsModel();
         $this->view = new ApiView();
     }
-    function getAll(){
-        $travels = $this->model->getTravels();
-        $this->view->response($travels, 200);
+    function get($params = [])
+    {
+        $id = $params[':ID'];
+        if (empty($params)) {
+            $travels = $this->model->getTravels();
+            $this->view->response($travels, 200);
+        } else {
+            $travel = $this->model->getDetailsById($id);
+            var_dump($travel);
+            if ($travel) {
+                $this->view->response($travel, 200);
+            } else {
+                $this->view->response(
+                    ['error' => 'la tarea con el id ' . $id . 'no existe'],
+                    404
+                );
+            }
+        }
+    }
+    function create(){
+        
     }
 }
--- router.php
+++ router.php
@@ -4,8 +4,8 @@ require_once './app/controller/travels.api.controller.php';
 
 $router = new Router();
 
-//                 endpoint  metodo   controller            funcion
-$router->addRoute('viajes', 'GET', 'TravelsApiController', 'getAll');
-
+//                 endpoint      verbo   controller            funcion
+$router->addRoute('viajes',     'GET', 'TravelsApiController', 'get');
+$router->addRoute('viajes/:ID', 'GET', 'TravelsApiController', 'get');
 
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
